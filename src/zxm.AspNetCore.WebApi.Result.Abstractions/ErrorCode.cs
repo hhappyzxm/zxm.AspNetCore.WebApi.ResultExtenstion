bb257de0a178cc945f6a5106483ec5a2fa074c3e
zxm <hhappyzxm@163.com>
2016-08-15 14:44:21 +0800

    Add error code and default error message

--- src/zxm.AspNetCore.WebApi.Result.Abstractions/ErrorCode.cs
+++ src/zxm.AspNetCore.WebApi.Result.Abstractions/ErrorCode.cs
@@ -9,10 +9,15 @@
         InvalidContentType = 2000,
 
         InvalidSignature = 3000,
-        InvalidClientId = 3001,
+        MissingClientId = 3001,
+        InvalidClientId = 3002,
+        MissingSignature = 3003,
+        MissingTimestamp = 3004,
+        TimestampExpired = 3005,
 
-        InvalidLoggedUserToken = 4000,
-        LoggedUserTokenExpired = 4001,
+        MissingUserAccessToken = 4000,
+        InvalidUserAccessToken = 4001,
+        UserAccessTokenExpired = 4002,
 
         InternalServerError = 5000
     }
---
+++ src/zxm.AspNetCore.WebApi.Result.Abstractions/ErrorMessageFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace zxm.AspNetCore.WebApi.Result.Abstractions
+{
+    public static class ErrorMessageFactory
+    {
+        public static string GetErrorMessage(ErrorCode errorCode)
+        {
+            switch (errorCode)
+            {
+                case ErrorCode.InvalidHttpMethod:
+                    return "invalid http method";
+                case ErrorCode.InvalidContentType:
+                    return "invalid content";
+                case ErrorCode.InvalidSignature:
+                    return "invalid signature";
+                case ErrorCode.MissingClientId:
+                    return "missing client id";
+                case ErrorCode.InvalidClientId:
+                    return "invalid client id";
+                case ErrorCode.MissingSignature:
+                    return "missing signature";
+                case ErrorCode.MissingTimestamp:
+                    return "missing timestamp";
+                case ErrorCode.TimestampExpired:
+                    return "timestamp expired";
+                case ErrorCode.MissingUserAccessToken:
+                    return "missing user access token";
+                case ErrorCode.InvalidUserAccessToken:
+                    return "invalid user access token";
+                case ErrorCode.UserAccessTokenExpired:
+                    return "user access token expired";
+                case ErrorCode.InternalServerError:
+                    return "internal server error";
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
