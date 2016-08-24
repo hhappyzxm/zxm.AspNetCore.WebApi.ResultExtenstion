8a787c576e83a79d3777e482663e39799e08c1fd
zxm <hhappyzxm@163.com>
2016-08-24 12:36:48 +0800

    Move NonSystemException

--- src/zxm.AspNetCore.WebApi.Result.Abstractions/NonSystemException.cs
+++ src/zxm.AspNetCore.WebApi.Result.Abstractions/NonSystemException.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace zxm.AspNetCore.WebApi.ResultExtenstion
+namespace zxm.AspNetCore.WebApi.Result.Abstractions
 {
     public class NonSystemException : Exception
     {
